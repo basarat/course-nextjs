1e5d9b66a4bfa9fd39724fb7ec7f789a186e8b99
basarat <basaratali@gmail.com>
2024-09-16 09:40:32 +1000

    better

--- lessons/why_rsc/src/app/page.tsx
+++ lessons/why_rsc/src/app/page.tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 
+/** Normally in order to use this in the frontend you would need to expose it as an API */
 async function readme() {
   const contents = await fs.readFileSync(
     path.join(process.cwd(), "/README.md")
@@ -8,6 +9,10 @@ async function readme() {
   return contents.toString("utf-8");
 }
 
+/** 
+ * React Server Components or RSCs
+ * allow you to use server functions in a server component with async/await
+ */
 export default async function Fancy() {
   const contents = await readme();
   return <>{contents}</>;
