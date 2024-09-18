d1efa0050f5f4665032a27f47fc08605ac98f041
basarat <basaratali@gmail.com>
2024-09-18 13:20:47 +1000

    better

--- lessons/nesting/src/app/client/page.tsx
+++ lessons/nesting/src/app/client/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { ClientComponent } from "../ui/client";
+import { UnmarkedComponent } from "../ui/unmarked";
+
 export default function ClientPage() {
   console.log("Page: Client");
   return (
     <>
       <h1>Client Page</h1>
+      <ClientComponent />
+      <UnmarkedComponent />
     </>
   );
 }
--- lessons/nesting/src/app/server/page.tsx
+++ lessons/nesting/src/app/server/page.tsx
@@ -1,15 +1,15 @@
-import { UnmarkedComponent } from "../ui/unmarked";
-import { ClientComponent } from "../ui/client";
 import { ServerComponent } from "../ui/server";
+import { ClientComponent } from "../ui/client";
+import { UnmarkedComponent } from "../ui/unmarked";
 
 export default function ServerPage() {
   console.log("Page: Server");
   return (
     <>
       <h1>Server Page</h1>
+      <ServerComponent />
       <ClientComponent />
       <UnmarkedComponent />
-      <ServerComponent />
     </>
   );
 }
